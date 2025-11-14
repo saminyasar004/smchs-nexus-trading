11ffc4f7e6d58e0a80318210472aaaee7e18ce90
Samin Yasar <yasarsamin57@gmail.com>
2025-11-14 12:22:07 +0600

    dev: project route specified and configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
-// import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Index from "./pages/index";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Careers from "./pages/Careers";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
+import CookiePolicy from "./pages/CookiePolicy";
+import Compliance from "./pages/Compliance";
 import NotFound from "./pages/NotFound";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-	<QueryClientProvider client={queryClient}>
-		<TooltipProvider>
-			{/* <Toaster /> */}
-			<Sonner />
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Index />} />
-					{/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</BrowserRouter>
-		</TooltipProvider>
-	</QueryClientProvider>
-);
+const App = () => {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<TooltipProvider>
+				<Toaster />
+				<Sonner />
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Index />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/services" element={<Services />} />
+						<Route path="/contact" element={<Contact />} />
+						<Route path="/careers" element={<Careers />} />
+						<Route
+							path="/privacy-policy"
+							element={<PrivacyPolicy />}
+						/>
+						<Route
+							path="/terms-of-service"
+							element={<TermsOfService />}
+						/>
+						<Route
+							path="/cookie-policy"
+							element={<CookiePolicy />}
+						/>
+						<Route path="/compliance" element={<Compliance />} />
+						{/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</BrowserRouter>
+			</TooltipProvider>
+		</QueryClientProvider>
+	);
+};
 
 export default App;
